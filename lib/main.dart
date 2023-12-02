ef090d3af193b8258edab3d85f5b698730a314f6
kamel <neilikamel@gmail.com>
2023-12-02 23:16:25 +0100

    add login features

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_project/core/routing/app_router.dart';
 import 'package:flutter_complete_project/doc_app.dart';
+
+import 'core/routing/app_router.dart';
 
 void main() {
   runApp(DocApp(
